83e91e20449f9a3f0d12658a2da4d8033eee48e5
Lapinas <mrlapinas99@gmail.com>
2022-02-16 12:04:52 +0000

    Added use so we can now see the outputs

--- src/server.js
+++ src/server.js
@@ -3,11 +3,12 @@ require("./dbconnection/connection");
 
 // Other imports
 const express = require("express");
-const movieRouter = require("./movies/movieRouters");
+const MR = require("./movies/movieRouters");
 
 // Setup
 const app = express();
 app.use(express.json());
+app.use(MR);
 const port = process.env.PORT;
 
 app.listen(port, () => {
